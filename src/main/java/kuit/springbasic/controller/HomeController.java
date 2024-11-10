53af48e2149fd863e415f27500f8af77926e227c
JoGyuBin <rbqls3529@gmail.com>
2024-11-10 14:13:27 +0900

    0단계 완료

--- src/main/java/kuit/springbasic/controller/HomeController.java
+++ src/main/java/kuit/springbasic/controller/HomeController.java
@@ -1,6 +1,25 @@
 package kuit.springbasic.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import kuit.springbasic.db.QuestionRepository;
+import kuit.springbasic.domain.Question;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Collection;
+
+@Slf4j
+@Controller
+@RequiredArgsConstructor
 public class HomeController {
+
+    private final QuestionRepository questionRepository;
 
     /**
      * TODO: showHome
@@ -9,4 +28,36 @@ public class HomeController {
      * showHomeV3 : parameter - Model / return - String
      */
 
+    @RequestMapping("/homeV1")
+    public ModelAndView showHomeV1(HttpServletRequest req, HttpServletResponse resp) {
+        log.info("showHomeV1");
+        ModelAndView mav = new ModelAndView("home");
+
+        Collection<Question> questions = questionRepository.findAll();
+        //mav.getModel().put("questions", questions);
+        mav.addObject("questions", questions);
+
+        return mav;
+    }
+
+    @RequestMapping("/homeV2")
+    public ModelAndView showHomeV1() {
+        log.info("showHomeV2");
+        ModelAndView mav = new ModelAndView("home");
+
+        Collection<Question> questions = questionRepository.findAll();
+        //mav.getModel().put("questions", questions);
+        mav.addObject("questions", questions);
+
+        return mav;
+    }
+
+    @RequestMapping("/")
+    public String showHomeV3(Model model) {
+        log.info("showHomeV3");
+        Collection<Question> questions = questionRepository.findAll();
+        model.addAttribute("questions", questions);
+
+        return "home";
+    }
 }
--- src/main/java/kuit/springbasic/controller/LoginController.java
+++ src/main/java/kuit/springbasic/controller/LoginController.java
@@ -1,14 +1,42 @@
 package kuit.springbasic.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import kuit.springbasic.db.UserRepository;
+import kuit.springbasic.domain.User;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Slf4j
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/user")
 public class LoginController {
+
+    private final UserRepository userRepository;
 
     /**
      * TODO: showLoginForm
      */
+    @RequestMapping("/loginForm")
+    public String showLoginForm() {
+        log.info("showLoginForm");
+        return "/user/login";
+    }
 
     /**
      * TODO: showLoginFailed
      */
+    @RequestMapping("/loginFailed")
+    public String showLoginFailed() {
+        log.info("showLoginFailed");
+        return "/user/loginFailed";
+    }
+
 
     /**
      * TODO: login
@@ -17,6 +45,52 @@ public class LoginController {
      * loginV3 : @RequestParam 생략(비추천)
      * loginV4 : @ModelAttribute
      */
+    //@RequestMapping("/user/login")
+    public String loginV1(@RequestParam("userId") String userId,
+                          @RequestParam("password") String password,
+                          HttpServletRequest req) {
+        log.info("loginV1");
+        User loggedInUser = new User(userId, password);
+        User user = userRepository.findByUserId(userId);
+
+        if (user != null && user.isSameUser(loggedInUser)) {
+            HttpSession session = req.getSession();
+            session.setAttribute("USER_SESSION_KEY", loggedInUser);
+            return "redirect:/";
+        }
+        return "redirect:/user/loginFailed";
+    }
+
+    //V2 버전은 오류가 발생함
+    //@RequestMapping("/user/login")
+    public String loginV2(@RequestParam String userId,
+                          @RequestParam String password,
+                          HttpServletRequest request) {
+        log.info("loginV2");
+        User loggedInUser = new User(userId, password);
+        User user = userRepository.findByUserId(userId);
+
+        if (user != null && user.isSameUser(loggedInUser)) {
+            HttpSession session = request.getSession();
+            session.setAttribute("USER_SESSION_KEY", loggedInUser);
+            return "redirect:/";
+        }
+        return "redirect:/user/loginFailed";
+    }
+
+    @RequestMapping("/login")
+    public String loginV2(@ModelAttribute User loggedInUser,
+                          HttpServletRequest request) {
+        log.info("loginV4");
+        User user = userRepository.findByUserId(loggedInUser.getUserId());
+
+        if (user != null && user.isSameUser(loggedInUser)) {
+            HttpSession session = request.getSession();
+            session.setAttribute("USER_SESSION_KEY", loggedInUser);
+            return "redirect:/";
+        }
+        return "redirect:/user/loginFailed";
+    }
 
     /**
      * TODO: logout
